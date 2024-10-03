9fb9fcdcc6b51f51c93fee010aa76a0671602f63
Sharad Kesarwani <108344822+sharadkesarwani@users.noreply.github.com>
2024-10-03 17:05:59 +0530

    executed prepare.sh

--- internal/controller/null/resource/zz_controller.go
+++ internal/controller/null/resource/zz_controller.go
@@ -21,8 +21,8 @@ import (
 	"github.com/pkg/errors"
 	ctrl "sigs.k8s.io/controller-runtime"
 
-	v1alpha1 "github.com/upbound/upjet-provider-template/apis/null/v1alpha1"
-	features "github.com/upbound/upjet-provider-template/internal/features"
+	v1alpha1 "github.com/spotinst/provider-crossplane-provider-spot/apis/null/v1alpha1"
+	features "github.com/spotinst/provider-crossplane-provider-spot/internal/features"
 )
 
 // Setup adds a controller that reconciles Resource managed resources.
