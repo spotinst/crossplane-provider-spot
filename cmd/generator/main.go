4fabd1c54af5c717b9bcb60b1bdc7975e575f348
Roi Kramer <roi.kramer@flexera.com>
2025-04-15 15:49:39 +0300

    use spotinst terraform sdk instead of forked cli

--- cmd/generator/main.go
+++ cmd/generator/main.go
@@ -12,6 +12,7 @@ import (
 	"github.com/crossplane/upjet/pkg/pipeline"
 
 	"github.com/spotinst/crossplane-provider-spot/config"
+	"github.com/spotinst/terraform-provider-spotinst/spotinst"
 )
 
 func main() {
@@ -23,5 +24,5 @@ func main() {
 	if err != nil {
 		panic(fmt.Sprintf("cannot calculate the absolute path with %s", rootDir))
 	}
-	pipeline.Run(config.GetProvider(), absRootDir)
+	pipeline.Run(config.GetProvider(spotinst.Provider(), true), absRootDir)
 }
--- config/oceangkevng/config.go
+++ config/oceangkevng/config.go
@@ -5,57 +5,15 @@ Copyright 2021 Upbound Inc.
 */
 
 import (
-	"encoding/base64"
-	"fmt"
-	"reflect"
-
 	ujconfig "github.com/crossplane/upjet/pkg/config"
 )
 
 // Configure configures the null group.
 func Configure(p *ujconfig.Provider) {
 	p.AddResourceConfigurator("spotinst_ocean_gke_launch_spec", func(r *ujconfig.Resource) {
-
 		// we need to override the default group that upjet generated for
 		// this resource, which would be "spotinst"
 		r.ShortGroup = "oceangkevng"
 		r.Kind = "OceanGkeVng"
-
-		r.TerraformConversions = append(r.TerraformConversions, &metadataConverter{})
 	})
-}
-
-var _ ujconfig.TerraformConversion = &metadataConverter{}
-
-type metadataConverter struct {
-}
-
-func (m *metadataConverter) Convert(params map[string]any, r *ujconfig.Resource, mode ujconfig.Mode) (map[string]any, error) {
-	if mode == ujconfig.FromTerraform {
-		metadata := params["metadata"].([]map[string]any)
-		fmt.Println("~~~~~", reflect.TypeOf(metadata).Name())
-
-		for _, mi := range metadata {
-			mi["value"] = base64.StdEncoding.EncodeToString([]byte(mi["value"].(string)))
-		}
-
-		params["metadata"] = metadata
-
-	} else if mode == ujconfig.ToTerraform {
-		metadata := params["metadata"].([]map[string]any)
-
-		for _, mi := range metadata {
-			decodeString, err := base64.StdEncoding.DecodeString(mi["value"].(string))
-			if err != nil {
-				fmt.Println("Error decoding base64 encoded string")
-				continue
-			}
-
-			mi["value"] = string(decodeString)
-		}
-
-		params["metadata"] = metadata
-	}
-
-	return params, nil
 }
