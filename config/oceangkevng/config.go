64ddc8b3ff99087c961640c3bc3b0d9daf030cbe
Roi Kramer <roi.kramer@flexera.com>
2025-04-15 10:05:08 +0300

    config change

--- config/oceangkevng/config.go
+++ config/oceangkevng/config.go
@@ -5,6 +5,10 @@ Copyright 2021 Upbound Inc.
 */
 
 import (
+	"encoding/base64"
+	"fmt"
+	"reflect"
+
 	ujconfig "github.com/crossplane/upjet/pkg/config"
 )
 
@@ -16,5 +20,42 @@ func Configure(p *ujconfig.Provider) {
 		// this resource, which would be "spotinst"
 		r.ShortGroup = "oceangkevng"
 		r.Kind = "OceanGkeVng"
+
+		r.TerraformConversions = append(r.TerraformConversions, &metadataConverter{})
 	})
+}
+
+var _ ujconfig.TerraformConversion = &metadataConverter{}
+
+type metadataConverter struct {
+}
+
+func (m *metadataConverter) Convert(params map[string]any, r *ujconfig.Resource, mode ujconfig.Mode) (map[string]any, error) {
+	if mode == ujconfig.FromTerraform {
+		metadata := params["metadata"].([]map[string]any)
+		fmt.Println("~~~~~", reflect.TypeOf(metadata).Name())
+
+		for _, mi := range metadata {
+			mi["value"] = base64.StdEncoding.EncodeToString([]byte(mi["value"].(string)))
+		}
+
+		params["metadata"] = metadata
+
+	} else if mode == ujconfig.ToTerraform {
+		metadata := params["metadata"].([]map[string]any)
+
+		for _, mi := range metadata {
+			decodeString, err := base64.StdEncoding.DecodeString(mi["value"].(string))
+			if err != nil {
+				fmt.Println("Error decoding base64 encoded string")
+				continue
+			}
+
+			mi["value"] = string(decodeString)
+		}
+
+		params["metadata"] = metadata
+	}
+
+	return params, nil
 }
